67cfde32b1027f5beed83fddcd333da129f67826
ouchzsc <ouchzsc@gmail.com>
2020-09-25 11:38:47 +0800

    场景不适合用ASyncObject，因为场景有排他性。改成监听unity场景回调

--- RazorClient/Assets/Scripts/Scene/SceneUtils.cs
+++ RazorClient/Assets/Scripts/Scene/SceneUtils.cs
@@ -6,16 +6,32 @@ public class SceneUtils
 {
     public static void loadScene(String sceneName, Action done)
     {
-        Boot.inst.StartCoroutine(SwitchSceneCoroutine(sceneName, done));
+        Boot.inst.StartCoroutine(LoadSceneCoroutine(sceneName, done));
     }
 
-    private static IEnumerator SwitchSceneCoroutine(string sceneName, Action done)
+    private static IEnumerator LoadSceneCoroutine(string sceneName, Action done)
     {
         var oper = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
-        while (oper.isDone==false)
+        while (oper.isDone == false)
         {
             yield return null;
         }
-        done();
+
+        done?.Invoke();
+    }
+
+    public static void unloadScene(string sceneName)
+    {
+        SceneManager.UnloadSceneAsync(sceneName);
+    }
+
+    public static void listenSceneUnload(Action<string> action)
+    {
+        SceneManager.sceneUnloaded += scene => action?.Invoke(scene.name);
+    }
+
+    public static void listenSceneLoaded(Action<string> action)
+    {
+        SceneManager.sceneLoaded += (scene, loadMode) => action?.Invoke(scene.name);
     }
 }
