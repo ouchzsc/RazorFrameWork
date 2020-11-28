7c553e1591c354e457aba89ee7b493084dc67ac5
ouchzsc <ouchzsc@gmail.com>
2020-11-29 02:04:35 +0800

    trunk/RazorClient/Lua/bullet/Bullet.lua

--- RazorClient/Lua/bullet/Bullet.lua
+++ RazorClient/Lua/bullet/Bullet.lua
@@ -1,18 +1,19 @@
 local ASyncGameObject = require("obj.ASyncGameObject")
 local module = require("module")
-local pos = { x = 0, y = 0, z = 0 }
 
 ---@class Bullet:ASyncGameObject
 local Bullet = ASyncGameObject:extends()
 
 function Bullet:setPos(x, y, z)
+    self.pos = self.pos or {}
+    local pos = self.pos
     pos.x = x
     pos.y = y
     pos.z = z
 end
 
 function Bullet:onEnable(gameObject)
-    gameObject.transform.position = pos
+    gameObject.transform.position = self.pos
     self:scheduleTimer("hideMe", "2", self.hide)
 end
 
